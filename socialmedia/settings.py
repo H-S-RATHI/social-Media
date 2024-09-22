926321974bc2444907d7768badee1073d10ca291
H.S.RATHI <goluu.000111@gmail.com>
2024-09-22 16:42:31 +0530

    update for vercel

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wvu!g!4!97#)x(nu^+isfgo27*ljn$9#8y^+zu^2#50c^h05r9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
